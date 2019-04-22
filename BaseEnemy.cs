f289a081a87dac61648c51517ed48c6a0162e543
k1llxna <44785075+k1llxna@users.noreply.github.com>
2019-04-22 18:51:42 -0400

    Add files via upload

---
+++ BaseEnemy.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseEnemy : MonoBehaviour {
+
+    private bool movingRight = true;
+
+    public int hp;
+    public int pointValue;
+    public int atkDmg;
+    public int speed;
+    public int noSpeed;
+
+    public Transform groundDetection;
+    public GameObject deathEffect;
+
+	void Start () {
+        if (hp <= 0)
+        {
+            hp = 1;
+        }
+
+		if (atkDmg <= 0)
+        {
+            atkDmg = 1;
+        }
+
+        if (speed < 0)
+        {
+            speed = 1;
+        }
+
+    }
+
+    void Update () {
+        Patrol();
+	}
+
+    public void Patrol()
+    {
+        // wall flip checks
+        transform.Translate(-Vector2.right * speed * Time.deltaTime);
+        RaycastHit2D groundInfo = Physics2D.Raycast(groundDetection.position, Vector2.down, 10f);
+
+        if (groundInfo.collider == false)
+        {
+            if (movingRight == true)
+            {
+                transform.eulerAngles = new Vector3(0, -180, 0);
+                movingRight = false;
+            }
+            else
+            {
+                transform.eulerAngles = new Vector3(0, 0, 0);
+                movingRight = true;
+            }
+        }
+    }
+
+    // enemy contact flip checks
+    public void OnCollisionEnter2D(Collision2D c)
+    {
+        if (c.gameObject.CompareTag("Enemy"))
+        {
+            if (movingRight == true)
+            {
+                transform.eulerAngles = new Vector3(0, -180, 0);
+                movingRight = false;
+            }
+            else
+            {
+                transform.eulerAngles = new Vector3(0, 0, 0);
+                movingRight = true;
+            }
+        }
+
+        if (c.gameObject.CompareTag("Projectile"))
+        {
+            Die();
+        }
+    }
+
+    public void Die()
+    {
+        FindObjectOfType<AudioManager>().Play("Hit");
+        Instantiate(deathEffect, transform.position, transform.rotation);
+        Destroy(gameObject);
+    }
+}
---
+++ Camera.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour {
+
+    Transform target;               // Thing for camera to follow
+    public Transform camBoundMin;   // Lowest and leftest value camera can move
+    public Transform camBoundMax;   // Highest and rightest value camera can move
+
+    float xMin, xMax, yMin, yMax;
+    
+    void Start()
+    {
+        GameObject go = GameObject.FindWithTag("Player");
+        if (!go)
+        {
+            Debug.Log("Player not found.");
+            return;
+        }
+
+        target = go.GetComponent<Transform>();
+
+        // Uses GameObjects to set the min and max values for camera movement
+        // - Update if 'Player' can move in different ranges of level
+        xMin = camBoundMin.position.x;
+        yMin = camBoundMin.position.y;
+
+        xMax = camBoundMax.position.x;
+        yMax = camBoundMax.position.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target)
+        {
+            // Change camera position to follow player, if player is in range
+            // - Mathf.Clamp, keeps first argument within range of second(min) and third(max)
+            transform.position = new Vector3(
+                Mathf.Clamp(target.position.x, xMin, xMax),
+                Mathf.Clamp(target.position.y, yMin, yMax),
+                transform.position.z);
+        }
+
+    }
+}
